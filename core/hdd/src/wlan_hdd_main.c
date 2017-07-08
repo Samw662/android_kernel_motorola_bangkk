OnePlusOSS/android_kernel_oneplus_msm8998

qcacld-3.0: Add .ini configuration for max number of AMSDU
qcacld-2.0 to qcacld-3.0 propagation

Host driver can send number of max AMSDUs in transmit frames.
Added a call to process_wma_set_command() similar to CLI
command processing for GEN_VDEV_PARAM_AMSDU during device
initialization.
Changed name of the field from isAmsduSupportInAMPDU to max_amsdu_num
in several places. Existing SME and LIM code passes this parameter
to WMA, but it is not used there. It may be useful if AMSDU setting
is changed to per-vdev basis in future.

WCNSS_qcom_cfg.ini changes:
Removed - "gAmsduSupportInAMPDU"
Added - Name = "gMaxAmsduNum", Min = 0, Max = 3, Default = 1

Actual function call to ol_txrx_aggr_cfg() send the parameters to
firmware is commented out because it is not implemented in firmware.

Change-Id: I2dfdd8c8c4a8807596ff28eee192ef66ac425653
CRs-Fixed: 871686

core/hdd/src/wlan_hdd_main.c
@@ -3126,6 +3126,17 @@ int hdd_set_fw_params(hdd_adapter_t *adapter)
 			hdd_err("FAILED TO SET RTSCTS Profile ret:%d", ret);
 			goto error;
 		}
+
+		hdd_info("SET AMSDU num %d", hdd_ctx->config->max_amsdu_num);
+
+		ret = wma_cli_set_command(adapter->sessionId,
+					  GEN_VDEV_PARAM_AMSDU,
+					  hdd_ctx->config->max_amsdu_num,
+					  GEN_CMD);
+		if (ret != 0) {
+			hdd_err("GEN_VDEV_PARAM_AMSDU set failed %d", ret);
+			goto error;
+		}
 	}
 
 	hdd_set_fw_log_params(hdd_ctx, adapter);

Samw662/android_kernel_motorola_bangkk

core/hdd/src/wlan_hdd_main.c
@@ -3638,6 +3638,17 @@ int hdd_set_fw_params(hdd_adapter_t *adapter)
 		goto error;
 	}
 
+	hdd_debug("SET AMSDU num %d", hdd_ctx->config->max_amsdu_num);
+
+	ret = wma_cli_set_command(adapter->sessionId,
+				  GEN_VDEV_PARAM_AMSDU,
+				  hdd_ctx->config->max_amsdu_num,
+				  GEN_CMD);
+	if (ret != 0) {
+		hdd_err("GEN_VDEV_PARAM_AMSDU set failed %d", ret);
+		goto error;
+	}
+
 	hdd_set_fw_log_params(hdd_ctx, adapter);
 	EXIT();
 