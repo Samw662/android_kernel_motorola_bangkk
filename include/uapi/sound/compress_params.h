crevanthsv/kernel_xiaomi_daisy

ALSA: compress: Add APTX format support in ALSA
Extend ALSA (Advanced Linux Sound Architecture) compress
to support APTX format.

CRs-Fixed: 1106128
Change-Id: I301c3fac1f0e267ca82f20a42437bae86a22413c
Signed-off-by: Dhanalakshmi Siddani <dsiddani@codeaurora.org>

include/uapi/sound/compress_params.h
@@ -103,7 +103,8 @@
 #define SND_AUDIOCODEC_ALAC                  ((__u32) 0x00000019)
 #define SND_AUDIOCODEC_APE                   ((__u32) 0x00000020)
 #define SND_AUDIOCODEC_DSD                   ((__u32) 0x00000021)
-#define SND_AUDIOCODEC_MAX                   SND_AUDIOCODEC_DSD
+#define SND_AUDIOCODEC_APTX                  ((__u32) 0x00000022)
+#define SND_AUDIOCODEC_MAX                   SND_AUDIOCODEC_APTX
 /*
  * Profile and modes are listed with bit masks. This allows for a
  * more compact representation of fields that will not evolve
@@ -396,6 +397,12 @@ struct snd_dec_ape {
 	__u32 seek_table_present;
 };
 
+struct snd_dec_aptx {
+	__u32 lap;
+	__u32 uap;
+	__u32 nap;
+};
+
 union snd_codec_options {
 	struct snd_enc_wma wma;
 	struct snd_enc_vorbis vorbis;
@@ -407,6 +414,7 @@ union snd_codec_options {
 	struct snd_dec_vorbis vorbis_dec;
 	struct snd_dec_alac alac;
 	struct snd_dec_ape ape;
+	struct snd_dec_aptx aptx_dec;
 } __attribute__((packed, aligned(4)));
 
 /** struct snd_codec_desc - description of codec capabilities

Samw662/android_kernel_motorola_bangkk

include/uapi/sound/compress_params.h
@@ -86,7 +86,8 @@
 #define SND_AUDIOCODEC_ALAC                  ((__u32) 0x00000010)
 #define SND_AUDIOCODEC_APE                   ((__u32) 0x00000011)
 #define SND_AUDIOCODEC_DSD                   ((__u32) 0x00000012)
-#define SND_AUDIOCODEC_MAX                   SND_AUDIOCODEC_DSD
+#define SND_AUDIOCODEC_APTX                  ((__u32) 0x00000013)
+#define SND_AUDIOCODEC_MAX                   SND_AUDIOCODEC_APTX
 
 /*
  * Profile and modes are listed with bit masks. This allows for a
@@ -380,6 +381,12 @@ struct snd_dec_wma {
 	__u32 avg_bit_rate;
 } __attribute__((packed, aligned(4)));
 
+struct snd_dec_aptx {
+	__u32 lap;
+	__u32 uap;
+	__u32 nap;
+};
+
 union snd_codec_options {
 	struct snd_enc_wma wma;
 	struct snd_enc_vorbis vorbis;
@@ -391,6 +398,7 @@ union snd_codec_options {
 	struct snd_dec_alac alac_dec;
 	struct snd_dec_ape ape_dec;
 	struct snd_dec_wma wma_dec;
+	struct snd_dec_aptx aptx_dec;
 } __attribute__((packed, aligned(4)));
 
 /** struct snd_codec_desc - description of codec capabilities