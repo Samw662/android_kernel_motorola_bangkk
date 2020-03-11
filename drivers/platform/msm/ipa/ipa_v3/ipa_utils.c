MotorolaMobilityLLC/kernel-msm

msm: ipa3: Updating SRAM locations for lito
Updating SRAM locations according to excel sheet for lito target.

Change-Id: I39f28bd8d706b483cb71694dfcb0d8183a419a49
Signed-off-by: sivakanth reddy vaka <svaka@codeaurora.org>

drivers/platform/msm/ipa/ipa_v3/ipa_utils.c
@@ -3201,21 +3201,21 @@ static struct ipa3_mem_partition ipa_4_7_mem_part = {
 	.apps_hdr_size			= 0x200,
 	.apps_hdr_size_ddr		= 0x800,
 	.modem_hdr_proc_ctx_ofst	= 0xad0,
-	.modem_hdr_proc_ctx_size	= 0x200,
-	.apps_hdr_proc_ctx_ofst		= 0xcd0,
+	.modem_hdr_proc_ctx_size	= 0xAC0,
+	.apps_hdr_proc_ctx_ofst		= 0x1590,
 	.apps_hdr_proc_ctx_size		= 0x200,
 	.apps_hdr_proc_ctx_size_ddr	= 0x0,
-	.nat_tbl_ofst			= 0xee0,
+	.nat_tbl_ofst			= 0x17A0,
 	.nat_tbl_size			= 0x800,
-	.nat_index_tbl_ofst		= 0x16e0,
+	.nat_index_tbl_ofst		= 0x1FA0,
 	.nat_index_tbl_size		= 0x100,
-	.nat_exp_tbl_ofst		= 0x17e0,
+	.nat_exp_tbl_ofst		= 0x20A0,
 	.nat_exp_tbl_size		= 0x400,
-	.pdn_config_ofst		= 0x1be8,
+	.pdn_config_ofst		= 0x24A8,
 	.pdn_config_size		= 0x50,
-	.stats_quota_ofst		= 0x1c40,
+	.stats_quota_ofst		= 0x2500,
 	.stats_quota_size		= 0x78,
-	.stats_tethering_ofst		= 0x1cb8,
+	.stats_tethering_ofst		= 0x2578,
 	.stats_tethering_size		= 0x238,
 	.stats_flt_v4_ofst		= 0,
 	.stats_flt_v4_size		= 0,
@@ -3225,29 +3225,29 @@ static struct ipa3_mem_partition ipa_4_7_mem_part = {
 	.stats_rt_v4_size		= 0,
 	.stats_rt_v6_ofst		= 0,
 	.stats_rt_v6_size		= 0,
-	.stats_fnr_ofst			= 0x1ef0,
+	.stats_fnr_ofst			= 0x27B0,
 	.stats_fnr_size			= 0x0,
-	.stats_drop_ofst		= 0x1ef0,
+	.stats_drop_ofst		= 0x27B0,
 	.stats_drop_size		= 0x20,
 	.modem_comp_decomp_ofst		= 0x0,
 	.modem_comp_decomp_size		= 0x0,
-	.modem_ofst			= 0x1f18,
-	.modem_size			= 0x100c,
-	.apps_v4_flt_hash_ofst	= 0x1f18,
+	.modem_ofst			= 0x27D8,
+	.modem_size			= 0x800,
+	.apps_v4_flt_hash_ofst	= 0x27B0,
 	.apps_v4_flt_hash_size	= 0x0,
-	.apps_v4_flt_nhash_ofst	= 0x1f18,
+	.apps_v4_flt_nhash_ofst	= 0x27B0,
 	.apps_v4_flt_nhash_size	= 0x0,
-	.apps_v6_flt_hash_ofst	= 0x1f18,
+	.apps_v6_flt_hash_ofst	= 0x27B0,
 	.apps_v6_flt_hash_size	= 0x0,
-	.apps_v6_flt_nhash_ofst	= 0x1f18,
+	.apps_v6_flt_nhash_ofst	= 0x27B0,
 	.apps_v6_flt_nhash_size	= 0x0,
-	.apps_v4_rt_hash_ofst	= 0x1f18,
+	.apps_v4_rt_hash_ofst	= 0x27B0,
 	.apps_v4_rt_hash_size	= 0x0,
-	.apps_v4_rt_nhash_ofst	= 0x1f18,
+	.apps_v4_rt_nhash_ofst	= 0x27B0,
 	.apps_v4_rt_nhash_size	= 0x0,
-	.apps_v6_rt_hash_ofst	= 0x1f18,
+	.apps_v6_rt_hash_ofst	= 0x27B0,
 	.apps_v6_rt_hash_size	= 0x0,
-	.apps_v6_rt_nhash_ofst	= 0x1f18,
+	.apps_v6_rt_nhash_ofst	= 0x27B0,
 	.apps_v6_rt_nhash_size	= 0x0,
 	.uc_descriptor_ram_ofst	= 0x3000,
 	.uc_descriptor_ram_size	= 0x0000,

Samw662/android_kernel_motorola_bangkk

drivers/platform/msm/ipa/ipa_v3/ipa_utils.c
@@ -3392,19 +3392,19 @@ static struct ipa3_mem_partition ipa_4_7_mem_part = {
 	.apps_hdr_size			= 0x200,
 	.apps_hdr_size_ddr		= 0x800,
 	.modem_hdr_proc_ctx_ofst	= 0xad0,
-	.modem_hdr_proc_ctx_size	= 0x200,
-	.apps_hdr_proc_ctx_ofst		= 0xcd0,
+	.modem_hdr_proc_ctx_size	= 0xAC0,
+	.apps_hdr_proc_ctx_ofst		= 0x1590,
 	.apps_hdr_proc_ctx_size		= 0x200,
 	.apps_hdr_proc_ctx_size_ddr	= 0x0,
-	.nat_tbl_ofst			= 0xee0,
-	.nat_tbl_size			= 0xd00,
-	.pdn_config_ofst		= 0x1be8,
+	.nat_tbl_ofst			= 0x17A0,
+	.nat_tbl_size			= 0x800,
+	.pdn_config_ofst		= 0x24A8,
 	.pdn_config_size		= 0x50,
-	.stats_quota_q6_ofst		= 0x1c40,
+	.stats_quota_q6_ofst		= 0x2500,
 	.stats_quota_q6_size		= 0x30,
-	.stats_quota_ap_ofst		= 0x1c70,
+	.stats_quota_ap_ofst		= 0x2530,
 	.stats_quota_ap_size		= 0x48,
-	.stats_tethering_ofst		= 0x1cb8,
+	.stats_tethering_ofst		= 0x2578,
 	.stats_tethering_size		= 0x238,
 	.stats_flt_v4_ofst		= 0,
 	.stats_flt_v4_size		= 0,
@@ -3414,29 +3414,29 @@ static struct ipa3_mem_partition ipa_4_7_mem_part = {
 	.stats_rt_v4_size		= 0,
 	.stats_rt_v6_ofst		= 0,
 	.stats_rt_v6_size		= 0,
-	.stats_fnr_ofst			= 0x1ef0,
+	.stats_fnr_ofst			= 0x27B0,
 	.stats_fnr_size			= 0x0,
-	.stats_drop_ofst		= 0x1ef0,
+	.stats_drop_ofst		= 0x27B0,
 	.stats_drop_size		= 0x20,
 	.modem_comp_decomp_ofst		= 0x0,
 	.modem_comp_decomp_size		= 0x0,
-	.modem_ofst			= 0x1f18,
-	.modem_size			= 0x100c,
-	.apps_v4_flt_hash_ofst	= 0x1f18,
+	.modem_ofst			= 0x27D8,
+	.modem_size			= 0x800,
+	.apps_v4_flt_hash_ofst	= 0x27B0,
 	.apps_v4_flt_hash_size	= 0x0,
-	.apps_v4_flt_nhash_ofst	= 0x1f18,
+	.apps_v4_flt_nhash_ofst	= 0x27B0,
 	.apps_v4_flt_nhash_size	= 0x0,
-	.apps_v6_flt_hash_ofst	= 0x1f18,
+	.apps_v6_flt_hash_ofst	= 0x27B0,
 	.apps_v6_flt_hash_size	= 0x0,
-	.apps_v6_flt_nhash_ofst	= 0x1f18,
+	.apps_v6_flt_nhash_ofst	= 0x27B0,
 	.apps_v6_flt_nhash_size	= 0x0,
-	.apps_v4_rt_hash_ofst	= 0x1f18,
+	.apps_v4_rt_hash_ofst	= 0x27B0,
 	.apps_v4_rt_hash_size	= 0x0,
-	.apps_v4_rt_nhash_ofst	= 0x1f18,
+	.apps_v4_rt_nhash_ofst	= 0x27B0,
 	.apps_v4_rt_nhash_size	= 0x0,
-	.apps_v6_rt_hash_ofst	= 0x1f18,
+	.apps_v6_rt_hash_ofst	= 0x27B0,
 	.apps_v6_rt_hash_size	= 0x0,
-	.apps_v6_rt_nhash_ofst	= 0x1f18,
+	.apps_v6_rt_nhash_ofst	= 0x27B0,
 	.apps_v6_rt_nhash_size	= 0x0,
 	.uc_descriptor_ram_ofst	= 0x3000,
 	.uc_descriptor_ram_size	= 0x0000,