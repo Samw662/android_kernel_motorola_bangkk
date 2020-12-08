EmanuelCN/kernel_xiaomi_sm8250

msm: camera: ope: Add support to dynamic switch pix_pattern
Add support to dynamically switch pix_pattern of read clients
based on data received in each request.

CRs-Fixed: 2811530
Change-Id: Icb3ebd33cae59b8db87bc0011d6560492ad29c3a
Signed-off-by: Rishabh Jain <risjai@codeaurora.org>

include/uapi/media/cam_ope.h
@@ -104,7 +104,7 @@ struct ope_stripe_info {
  * @direction:     Direction of a buffer of a port(Input/Output)
  * @resource_type: Port type
  * @num_planes:    Number of planes for a port
- * @reserved:      Reserved
+ * @pix_pattern:   Pixel pattern for raw input
  * @num_stripes:   Stripes per plane
  * @mem_handle:    Memhandles of each Input/Output Port
  * @plane_offset:  Offsets of planes
@@ -120,7 +120,7 @@ struct ope_io_buf_info {
 	uint32_t direction;
 	uint32_t resource_type;
 	uint32_t num_planes;
-	uint32_t reserved;
+	uint32_t pix_pattern;
 	uint32_t num_stripes[OPE_MAX_PLANES];
 	uint32_t mem_handle[OPE_MAX_PLANES];
 	uint32_t plane_offset[OPE_MAX_PLANES];

Samw662/android_kernel_motorola_bangkk

include/uapi/camera/media/cam_ope.h
@@ -104,7 +104,7 @@ struct ope_stripe_info {
  * @direction:     Direction of a buffer of a port(Input/Output)
  * @resource_type: Port type
  * @num_planes:    Number of planes for a port
- * @reserved:      Reserved
+ * @pix_pattern:   Pixel pattern for raw input
  * @num_stripes:   Stripes per plane
  * @mem_handle:    Memhandles of each Input/Output Port
  * @plane_offset:  Offsets of planes
@@ -120,7 +120,7 @@ struct ope_io_buf_info {
 	__u32 direction;
 	__u32 resource_type;
 	__u32 num_planes;
-	__u32 reserved;
+	__u32 pix_pattern;
 	__u32 num_stripes[OPE_MAX_PLANES];
 	__u32 mem_handle[OPE_MAX_PLANES];
 	__u32 plane_offset[OPE_MAX_PLANES];