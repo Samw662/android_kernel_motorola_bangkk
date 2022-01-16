bd04869b2b3d5a575860c1115f80f8ed11e9e83a
Cosmin Tanislav <demonsingur@gmail.com>
2022-01-16 23:02:38 +0200

    scsi: ufs: Increase power state switch timeout
    Change-Id: I80ef3f6cf684cbd1b5bad54d463b5120979aa5e5

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -97,6 +97,10 @@ unsigned int ram_size;
 #endif
 
 unsigned int storage_mfrid;
+
+/*UIC PWR CTRL command timeout, unit: ms*/
+#define UIC_PWR_CTRL_TIMEOUT 3000
+
 /* NOP OUT retries waiting for NOP IN response */
 #define NOP_OUT_RETRIES    10
 /* Timeout after 30 msecs if NOP OUT hangs without response */
@@ -4224,7 +4228,7 @@ static int ufshcd_uic_pwr_ctrl(struct ufs_hba *hba, struct uic_command *cmd)
 	}
 
 	if (!wait_for_completion_timeout(hba->uic_async_done,
-					 msecs_to_jiffies(UIC_CMD_TIMEOUT))) {
+					 msecs_to_jiffies(UIC_PWR_CTRL_TIMEOUT))) {
 		dev_err(hba->dev,
 			"pwr ctrl cmd 0x%x with mode 0x%x completion timeout\n",
 			cmd->command, cmd->argument3);
