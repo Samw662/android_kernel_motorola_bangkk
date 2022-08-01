b861655fce8dd39683f8fb66e90d941c899ac633
Vaisakh Murali <mvaisakh@statixos.com>
2022-08-01 15:29:03 +0000

    techpack/display: Enforce ulps by default
    This will force enable ulps by default for all panels without checking
    for the dts boolean.
    
    Change-Id: Icdd6f7255cb7428a76a6e95f82474c89e6dd991d
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -2897,8 +2897,7 @@ static int dsi_panel_parse_misc_features(struct dsi_panel *panel)
 	int i, rc = 0;
 	u32 val = 0;
 
-	panel->ulps_feature_enabled =
-		utils->read_bool(utils->data, "qcom,ulps-enabled");
+	panel->ulps_feature_enabled = true;
 
 	DSI_DEBUG("%s: ulps feature %s\n", __func__,
 		(panel->ulps_feature_enabled ? "enabled" : "disabled"));
