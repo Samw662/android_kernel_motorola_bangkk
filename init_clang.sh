c07793596f35119e2c5c6683eb691221cb0f82d3
ghazzor <127341071+ghazzor@users.noreply.github.com>
2024-09-26 00:34:22 +0530

    CI: switch to neutron clang 18

--- init_clang.sh
+++ init_clang.sh
@@ -1,7 +1,7 @@
 mkdir -p toolchain
 cd toolchain
 echo 'Download antman and sync'
-bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") -S=11032023 # sync neutron clang 17
+bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") -S=05012024 # sync neutron clang 18
 echo 'Patch for glibc'
 bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") --patch=glibc
 echo 'Done'
