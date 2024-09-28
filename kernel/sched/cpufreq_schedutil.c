b8259a398010ddb67ef3a717d7241fa3e63e36b0
ghazzor <kartikhis8o@gmail.com>
2024-09-28 19:13:31 +0530

    Revert "cpufreq: schedutil: Disable frequency boosting"
    This reverts commit b501bc8a51e10f410d0670d9def482e0e3c58ebc.
    
    this makes the freq go from low freq to max , skipping freq in b/w
    
    Signed-off-by: ghazzor <kartikhis8o@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -382,7 +382,7 @@ unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
 	 * frequency will be gracefully reduced with the utilization decay.
 	 */
 	util = util_cfs + cpu_util_rt(rq);
-	if (type == FREQUENCY_UTIL && uclamp_rq_util_with(rq, util, p) < util)
+	if (type == FREQUENCY_UTIL)
 		util = uclamp_rq_util_with(rq, util, p);
 
 	dl_util = cpu_util_dl(rq);
